e0232e063b8c54a1ddc1843b632b9cee0e884777
BT_1N3_11 <vbondmoore6034@bths.edu>
2018-10-05 11:24:00 -0400

    Go commit Rope neck

---
+++ src/AMV/Teacher.java
@@ -0,0 +1,23 @@
+package AMV;
+
+public class Teacher {
+
+    public class Teacher extends Person {
+        private String Subject;
+        private String Title;
+
+        public Teacher(String Subject, String Title, String firstName, String familyName) {
+            super(firstName, familyName);
+            this.Subject = Subject;
+            this.Title = Title;
+        }
+
+        public String getSubject() {
+            return Subject;
+        }
+
+        public String toString() {
+            return this.Title + "." + " " + this.getFamilyName();
+        }
+    }
+}
